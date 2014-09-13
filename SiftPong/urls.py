ac0dd6a2fef6ef647c9c7bdf01d6606dc2ef7ee7
Yoav Schatzberg <yoav@siftscience.com>
2014-09-12 18:36:22 -0700

    removed pongranker from urls.

--- SiftPong/urls.py
+++ SiftPong/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pongranker.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^pongranker/', include('pongranker.urls')),
+    url(r'^', include('pongranker.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
