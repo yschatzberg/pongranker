f14f8bd8cacef99d638d76540ef06d1e82a47f6e
Yoav Schatzberg <yoav@siftscience.com>
2014-09-15 11:03:06 -0700

    Adding Postgres support for Heroku

--- SiftPong/settings.py
+++ SiftPong/settings.py
@@ -115,3 +115,28 @@ USE_TZ = True
 
 ## Static Files
 STATIC_URL = '/static/'
+
+
+
+
+# HEROKU STUFF
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
