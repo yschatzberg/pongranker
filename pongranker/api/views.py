55fa692f417d2b15d9d5d8c6f771d0c88bc977ab
Yoav Schatzberg <yoav@siftscience.com>
2015-03-05 10:07:20 -0800

    Changed how post_match works Added DoublesTeam and DoublesMatch objects to the model

--- pongranker/api/views.py
+++ pongranker/api/views.py
@@ -5,10 +5,9 @@ from django.template import RequestContext
 import json, time
 from elo import rate_1vs1
 
-from pongranker.models import Player, Match, MatchGame
+from pongranker.models import Player, Match, MatchGame, DoublesTeam, DoublesMatch
 from django.contrib.auth.models import User
-
-
+from pongranker.api import helper_functions
 def get_player_emails_and_names(request):
     player_list = User.objects.order_by('first_name', 'last_name')
 
@@ -19,6 +18,7 @@ def get_player_emails_and_names(request):
 
     return HttpResponse(json.dumps(response), content_type="application/json")
 
+# TODO: change this to get_singles_matches and implement get_doubles_matches once DoublesMatch exists.
 def get_matches(request):
 
     context = RequestContext(request)
@@ -36,10 +36,10 @@ def get_matches(request):
         match_json = {}
         match_json[u'player_1'] = match.player_1
         match_json[u'player_2'] = match.player_2
-        match_json[u'p1_point_change'] = match.p1_point_change
-        match_json[u'p2_point_change'] = match.p2_point_change
-        match_json[u'p1_wins'] = match.p1_wins
-        match_json[u'p2_wins'] = match.p2_wins
+        match_json[u'p1_point_change'] = match.team_1_point_change
+        match_json[u'p2_point_change'] = match.team_2_point_change
+        match_json[u'p1_wins'] = match.team_1_wins
+        match_json[u'p2_wins'] = match.team_2_wins
         match_json[u'timestamp'] = match.timestamp
 
         games_json = []
@@ -57,127 +57,131 @@ def get_matches(request):
 
     return HttpResponse(json.dumps(response), content_type="application/json")
 
+# This is the new one
 def post_match(request):
-
     if request.method == 'POST':
 
-      team_1 = json.loads(request.POST['team_1'])
-      team_2 = json.loads(request.POST['team_2'])
-
-      team_1_players = []
-      team_2_players = []
-
-      # get player objects [Player_object, player_object]
-      for player in team_1['players']:
-        team_1_players.append(Player.objects.get(user=User.objects.get(username=player)))
-      for player in team_2['players']:
-        team_2_players.append(Player.objects.get(user=User.objects.get(username=player)))
-
-      # sort the player lists by Player name (first + last)
-      for team in [team_1_players,team_2_players]:
-        team.sort(key=lambda x: (x.user.last_name,x.user.first_name))
+        team_1_json = json.loads(request.POST['team_1'])
+        team_2_json = json.loads(request.POST['team_2'])
 
 
-      # which team is inserted as player 1?
-      # generate full team name (for comparison)
-      team_1_full_name = ""
-      for player in team_1_players:
-        team_1_full_name += (player.user.first_name + player.user.last_name)
+        # Get team objects, might be a DoublesTeam, might be a Player
+        try:
+            team_1 = helper_functions.build_team_object(team_1_json)
+            team_2 = helper_functions.build_team_object(team_2_json)
+        except ValueError as e:
+            return HttpResponse(json.dumps({"error": 1, "message": str(e)}), content_type="application/json")
 
-      team_2_full_name = ""
-      for player in team_2_players:
-        team_2_full_name += (player.user.first_name + player.user.last_name)
+        # Check which team has priority and swap if necessary.
+        try:
+            if helper_functions.get_team_full_name(team_2) < helper_functions.get_team_full_name(team_1):
+                # flip lists of players so that team 1 is alphabetically superior
+                team_1, team_2 = team_2, team_1
+                # flip scores so that scores align with players
+                team_1_json['scores'], team_2_json['scores'] = team_2_json['scores'], team_1_json['scores']
+        except ValueError as e:
+            return HttpResponse(json.dumps({"error": 2, "message": str(e)}), content_type="application/json")
 
-      # if team 2 has a higher priority name
-      if team_2_full_name < team_1_full_name:
-        # flip lists of players so that team 1 is alphabetically superior
-        team_1_players,team_2_players = team_2_players,team_1_players
-        # flip scores so that scores align with players
-        team_1['scores'],team_2['scores'] = team_2['scores'],team_1['scores']
-      # scores are stored in team_1['scores']/team_2['scores']
+        team_1_player_list = helper_functions.get_team_player_list(team_1)
+        team_2_player_list = helper_functions.get_team_player_list(team_2)
 
-      # TODO: make this work for 2 v 2
-      match = Match(player_1=(team_1_players[0].user.first_name + " " +team_1_players[0].user.last_name),
-                    player_2=(team_2_players[0].user.first_name + " " +team_2_players[0].user.last_name))
-      match.timestamp = time.time()
-      match.full_clean()
-      match.save()
+        # IF either team has more than on player, create a DoublesMatch
+        if len(team_1_player_list) == 2 or len(team_2_player_list) == 2:
+            match = DoublesMatch(player_1=(team_1_player_list[0].user.first_name + " " + team_1_player_list[0].user.last_name),
+                    player_2=(team_2_player_list[0].user.first_name + " " + team_2_player_list[0].user.last_name))
 
-      game_list = []
-      for score in team_1['scores']:
-        game = MatchGame(score_1=score,
-                         #get index of score in team_1['scores'] and get the team_2['scores'] item at that same index
-                         score_2=team_2['scores'][team_1['scores'].index(score)])
-
-        game.full_clean()
-        game.save()
-        game_list.append(game)
-
-        old_ratings = (team_1_players[0].elo_singles_ranking, team_2_players[0].elo_singles_ranking)
-
-        # update ratings, total wins/losses, and calculate point change from game
-        if game.score_1 > game.score_2:
-          # Rankings
-          new_ratings = rate_1vs1(team_1_players[0].elo_singles_ranking, team_2_players[0].elo_singles_ranking)
-          team_1_players[0].elo_singles_ranking = round(new_ratings[0])
-          team_2_players[0].elo_singles_ranking = round(new_ratings[1])
-
-          #wins/losses for player and match
-          match.p1_wins += 1
-
-          # match point change
-          match.p1_point_change += team_1_players[0].elo_singles_ranking - old_ratings[0]
-          match.p2_point_change += team_2_players[0].elo_singles_ranking - old_ratings[1]
+            # Add second player to the team (if it exists).
+            if len(team_1_player_list) == 2:
+                match.player_3 = team_1_player_list[1].user.first_name + " " + team_1_player_list[1].user.last_name
+            if len(team_2_player_list) == 2:
+                match.player_4 = team_2_player_list[1].user.first_name + " " + team_2_player_list[1].user.last_name
         else:
-          new_ratings = rate_1vs1(team_2_players[0].elo_singles_ranking, team_1_players[0].elo_singles_ranking)
-          team_2_players[0].elo_singles_ranking = round(new_ratings[0])
-          team_1_players[0].elo_singles_ranking = round(new_ratings[1])
+            # Create Match object
+            match = Match(player_1=(team_1_player_list[0].user.first_name + " " + team_1_player_list[0].user.last_name),
+                    player_2=(team_2_player_list[0].user.first_name + " " + team_2_player_list[0].user.last_name))
 
-          match.p2_wins += 1
-          match.p1_point_change = team_1_players[0].elo_singles_ranking - old_ratings[1]
-          match.p2_point_change = team_2_players[0].elo_singles_ranking - old_ratings[0]
-
-      for g in game_list:
-        match.games.add(g)
-
-      match.save()
-
-      # Adding a win based on who won the match
-      if match.p1_wins > match.p2_wins:
-          team_1_players[0].total_singles_wins += 1
-          team_2_players[0].total_singles_losses += 1
-      else:
-          team_2_players[0].total_singles_wins += 1
-          team_1_players[0].total_singles_losses += 1
-
-      for team_players in [team_1_players, team_2_players]:
-        for player in team_players:
-          player.matches.add(match)
-          player.save()
+        match.timestamp = time.time()
+        match.full_clean()
+        match.save()
 
 
 
+        game_list = []
+        # for each score pare add a game
+        for score in team_1_json['scores']:
+            # Create game object
+            game = MatchGame(score_1=score,
+                         #get index of score in team_1['scores'] and get the team_2['scores'] item at that same index
+                         score_2=team_2_json['scores'][team_1_json['scores'].index(score)])
+
+            game.full_clean()
+            game.save()
+            # Add to game list
+            game_list.append(game)
+
+            # Grab old list for point change calculations
+            old_ratings = (team_1.elo_ranking, team_2.elo_ranking)
+
+            # update ratings, total wins/losses, and calculate point change from game
+            if game.score_1 > game.score_2:
+                # Calculate new ranking
+                new_ratings = rate_1vs1(team_1.elo_ranking, team_2.elo_ranking)
+                team_1.elo_ranking = round(new_ratings[0])
+                team_2.elo_ranking = round(new_ratings[1])
+
+                #wins/losses for player and match
+                match.team_1_wins += 1
+
+                # match point change
+                match.team_1_point_change = team_1.elo_ranking - old_ratings[0]
+                match.team_2_point_change = team_2.elo_ranking - old_ratings[1]
+            else:
+                new_ratings = rate_1vs1(team_2.elo_ranking, team_1.elo_ranking)
+                team_2.elo_ranking = round(new_ratings[0])
+                team_1.elo_ranking = round(new_ratings[1])
+
+                match.team_2_wins += 1
+                match.team_1_point_change = team_1.elo_ranking - old_ratings[1]
+                match.team_2_point_change = team_2.elo_ranking - old_ratings[0]
+
+        for g in game_list:
+            match.games.add(g)
+
+        match.save()
+
+        # Adding a win based on who won the match
+        if match.team_1_wins > match.team_2_wins:
+            team_1.total_wins += 1
+            team_2.total_losses += 1
+        else:
+            team_2.total_wins += 1
+            team_1.total_losses += 1
+
+        # If team is 1 player, only add it to them, otherwise, add to team object
+        for team in [team_1, team_2]:
+            team.matches.add(match)
+            team.save()
 
     return HttpResponse(json.dumps({"error": 0, "message": "OK"}), content_type="application/json")
 
-
+# TODO: Create get_teams_ranked
 def get_players_ranked(request):
 
     if request.method == 'GET':
       response_json = {}
-      player_list = Player.objects.order_by('-elo_singles_ranking','-total_singles_wins')
+      player_list = Player.objects.order_by('-elo_ranking','-total_wins')
 
       max = request.GET.get('max_results', len(player_list))
       max = int(max)
 
       response_json = []
       for player in player_list[:max]:
-        if (player.total_singles_wins + player.total_singles_losses) > 1:
+        if (player.total_wins + player.total_losses) > 1:
           player_json = {}
           player_json['name'] = player.user.first_name + " " + player.user.last_name
-          player_json['ranking'] = player.elo_singles_ranking
-          player_json['wins'] = player.total_singles_wins
-          player_json['losses'] = player.total_singles_losses
+          player_json['ranking'] = player.elo_ranking
+          player_json['wins'] = player.total_wins
+          player_json['losses'] = player.total_losses
 
           response_json.append(player_json)
 
--- pongranker/models.py
+++ pongranker/models.py
@@ -3,44 +3,87 @@ from django.contrib.auth.models import User
 import time
 import datetime
 
+
 class MatchGame(models.Model):
     # score for player 1 in the Match
-    score_1    = models.IntegerField()
+    score_1 = models.IntegerField()
     #score for player 2 in the Match
-    score_2    = models.IntegerField()
+    score_2 = models.IntegerField()
 
     def __unicode__(self):
-      return "\nTeam 1 Score: " + str(self.score_1) + \
-              "\nTeam 2 Score: " + str(self.score_2)
+        return "\nTeam 1 Score: " + str(self.score_1) + \
+               "\nTeam 2 Score: " + str(self.score_2)
 
 class Match(models.Model):
-    # first player, alphabetically.  i.e. arzav jain before yoav schatzberg
-    # this is constructed by concatenating user.first_name user.last_name[0] with a space
+    # Player name of player in Team 1
     player_1 = models.CharField(max_length=100)
 
+    # Player Name of Player in team 2
     player_2 = models.CharField(max_length=100)
 
-    p1_point_change = models.IntegerField(default=0)
-    p2_point_change = models.IntegerField(default=0)
-    p1_wins = models.IntegerField(default=0)
-    p2_wins = models.IntegerField(default=0)
+    # Number of points added or subtracted from team 1's ELO
+    team_1_point_change = models.IntegerField(default=0)
+    # Number of points added or subtracted from team 2's ELO
+    team_2_point_change = models.IntegerField(default=0)
+    # Number of wins Team 1 had this match
+    team_1_wins = models.IntegerField(default=0)
+    # Number of wins Team 2 had this match
+    team_2_wins = models.IntegerField(default=0)
 
+    # A list of games that occurred during this match.
     games = models.ManyToManyField(MatchGame)
 
+    # Time of match
     timestamp = models.BigIntegerField(default=0)
 
     def __unicode__(self):
-        output =  "\nTeam 1: " + self.player_1 + \
-                  "\nTeam 1 Point Change: " + str(self.p1_point_change) + \
-                  "\nTeam 2 Point Change: " + str(self.p2_point_change) + \
-                "\nTeam 2: " + self.player_2 + \
-                "\nTeam 1 Wins: " + str(self.p1_wins) + \
-                "\nTeam 2 Wins: " + str(self.p2_wins) + \
-                "\nMatch Date: " + datetime.datetime.fromtimestamp(self.timestamp).strftime('%Y-%m-%d')
+        output = "\nTeam 1: " + self.player_1 + \
+                 "\nTeam 1 Point Change: " + str(self.team_1_point_change) + \
+                 "\nTeam 2 Point Change: " + str(self.team_2_point_change) + \
+                 "\nTeam 2: " + self.player_2 + \
+                 "\nTeam 1 Wins: " + str(self.team_1_wins) + \
+                 "\nTeam 2 Wins: " + str(self.team_2_wins) + \
+                 "\nMatch Date: " + datetime.datetime.fromtimestamp(self.timestamp).strftime('%Y-%m-%d')
+
+
+class DoublesMatch(models.Model):
+    # first player in team 1, alphabetically.  i.e. arzav jain before yoav schatzberg
+    player_1 = models.CharField(max_length=100)
+    # second player in team 1
+    player_3 = models.CharField(max_length=100, default='', blank=True)
+
+    # first player in team 2, alphabetically.  i.e. arzav jain before yoav schatzberg
+    player_2 = models.CharField(max_length=100)
+    # second player in team 1
+    player_4 = models.CharField(max_length=100, default='', blank=True)
+
+    # Number of points added or subtracted from team 1's ELO
+    team_1_point_change = models.IntegerField(default=0)
+    # Number of points added or subtracted from team 2's ELO
+    team_2_point_change = models.IntegerField(default=0)
+    # Number of wins Team 1 had this match
+    team_1_wins = models.IntegerField(default=0)
+    # Number of wins Team 2 had this match
+    team_2_wins = models.IntegerField(default=0)
+
+    # A list of games that occurred during this match.
+    games = models.ManyToManyField(MatchGame)
+
+    # Time of match
+    timestamp = models.BigIntegerField(default=0)
+
+    def __unicode__(self):
+        output = "\nTeam 1: " + self.player_1 + " " + self.player_3 + \
+                 "\nTeam 1 Point Change: " + str(self.team_1_point_change) + \
+                 "\nTeam 2 Point Change: " + str(self.team_2_point_change) + \
+                 "\nTeam 2: " + self.player_2 + " " + self.player_4 + \
+                 "\nTeam 1 Wins: " + str(self.team_1_wins) + \
+                 "\nTeam 2 Wins: " + str(self.team_2_wins) + \
+                 "\nMatch Date: " + datetime.datetime.fromtimestamp(self.timestamp).strftime('%Y-%m-%d')
         i = 1
         for game in self.games.all():
-          output += "\nGame " + str(i) + game.__unicode__()
-          i = i+1
+            output += "\nGame " + str(i) + game.__unicode__()
+            i += 1
 
         return output
 
@@ -50,33 +93,70 @@ class Player(models.Model):
     # the users login
     user = models.OneToOneField(User)
 
-
     # total number of singles wins
-    total_singles_wins      = models.IntegerField(default=0)
+    total_wins = models.IntegerField(default=0)
 
     # total number of singles losses.
-    total_singles_losses    = models.IntegerField(default=0)
+    total_losses = models.IntegerField(default=0)
 
     # players Singles Ranking.
-    elo_singles_ranking         = models.IntegerField(default=1200)
+    elo_ranking = models.IntegerField(default=1200)
 
+    # a list of this players Singles Matches
     matches = models.ManyToManyField(Match)
+
+    # A list of this players doubles matches (i.e. 2 v 1's, this is the 1)
+    doubles_matches = models.ManyToManyField(DoublesMatch)
 
     def __unicode__(self):
         return "\nEmail: " + self.user.email + \
                "\nName: " + self.user.first_name + " " + self.user.last_name + \
-               "\nRanking: " + str(self.elo_singles_ranking) + \
-               "\nTotal Wins: " + str(self.total_singles_wins) + \
-               "\nTotal Losses: " + str(self.total_singles_losses) + "\n"
+               "\nRanking: " + str(self.elo_ranking) + \
+               "\nTotal Wins: " + str(self.total_wins) + \
+               "\nTotal Losses: " + str(self.total_losses) + "\n"
+
+
+# Team object, This is used for doubles and singles
+class DoublesTeam(models.Model):
+
+    # first player, alphabetically.  i.e. arzav jain before yoav schatzberg
+    players = models.ManyToManyField(Player)
+
+    # Total wins for this team
+    total_wins = models.IntegerField(default=0)
+
+    # Total Losses for this team
+    total_losses = models.IntegerField(default=0)
+
+    # Teams Doubles Ranking.
+    elo_ranking = models.IntegerField(default=1200)
+
+    # The team name.
+    team_name = models.CharField(max_length=100, default="")
+
+    #matches for this team
+    matches = models.ManyToManyField(Match)
+
+    def __unicode__(self):
+        output = "\nTeam Name: " + self.team_name + \
+                 "\nPlayers: "
+
+        for player in self.players.all():
+            output += "\n  " + player.user.first_name + " " + player.user.last_name
+
+        output += "\nRanking: " + str(self.elo_ranking) + \
+                  "\nTotal Wins: " + str(self.total_wins) + \
+                  "\nTotal Losses: " + str(self.total_losses) + "\n"
+        return output
 
 
 class SinglesMatchup(models.Model):
     # Hash of the players names in the form word1Word2Word3_word1Word2Word3 in alphabetical order of names
     # (e.g. benAsherKlein_yoavSchatzberg)
-    name_hash       = models.CharField(max_length=100, primary_key=True)
+    name_hash = models.CharField(max_length=100, primary_key=True)
 
     # the list of players involved in this matchup.  always added alphabetically
-    players         = models.ManyToManyField(Player)
+    players = models.ManyToManyField(Player)
 
     def __unicode__(self):
         output = "\nName Hash: " + self.name_hash
@@ -96,11 +176,11 @@ class SinglesMatchup(models.Model):
 class SinglesMatchupScore(models.Model):
 
     matchup = models.ForeignKey(SinglesMatchup)
-    name    = models.CharField(max_length=50, primary_key=True)
-    wins    = models.IntegerField(default=0)
+    name = models.CharField(max_length=50, primary_key=True)
+    wins = models.IntegerField(default=0)
 
     def __unicode__(self):
-        return  "\nMatchup: " + self.matchup.name_hash + \
-                "\nName: " + self.name + \
-                "\nWins: " + str(self.wins)
+        return "\nMatchup: " + self.matchup.name_hash + \
+               "\nName: " + self.name + \
+               "\nWins: " + str(self.wins)
 
